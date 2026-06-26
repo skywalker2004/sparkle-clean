dce27f0d2ab5e088d4207117df0df1f367161dc1
skywalker2004 <kelvinkiprono659@gmail.com>
2026-06-26 13:24:33 +0300

    add confirmBooking function: introduce confirmBooking method in booking controller for enhanced booking management

--- backend/dist/controllers/booking.controller.d.ts
+++ backend/dist/controllers/booking.controller.d.ts
@@ -3,5 +3,6 @@ import { AuthRequest } from '../types';
 export declare const createBooking: (req: any, res: Response) => Promise<Response<any, Record<string, any>>>;
 export declare const getBookings: (req: AuthRequest, res: Response) => Promise<void>;
 export declare const getBooking: (req: any, res: Response) => Promise<Response<any, Record<string, any>>>;
+export declare const confirmBooking: (req: AuthRequest, res: Response) => Promise<Response<any, Record<string, any>>>;
 export declare const updateBookingStatus: (req: AuthRequest, res: Response) => Promise<Response<any, Record<string, any>>>;
 //# sourceMappingURL=booking.controller.d.ts.map
